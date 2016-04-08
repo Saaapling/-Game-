5549edb7e3b1d2ccf324df400daa4bce6e81e7fa
Nicholas Zou <asdfjklundefined@gmail.com>
2016-04-08 10:34:52 -0400

    wagwe

--- ~Game~/src/Board.java
+++ ~Game~/src/Board.java
@@ -1,4 +1,13 @@
+import javax.swing.JPanel;
 
-public class Board {
 
+public class Board extends JPanel{
+	
+	public int[][] board = new int[300][1000];  //0-Empty, 1 terrain, 2 shot values, 10+ players
+	
+	public Board(){
+		
+	}
+	
+	
 }
