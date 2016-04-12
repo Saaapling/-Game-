7bf5a6d599da35c6945765a5c37ab82db1f734da
Nicholas Zou <asdfjklundefined@gmail.com>
2016-04-12 13:10:23 -0400

    yAQ3DSF

--- ~Game~/src/Board.java
+++ ~Game~/src/Board.java
@@ -1,30 +1,42 @@
+import java.awt.Color;
 import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 
 public class Board extends JPanel{
 	
-	public int[][] board = new int[1200][500];  //0-Empty, 1 terrain, 2 shot values, 10+ players
+	public int[][] board = new int[600][250];  //0-Empty, 1 terrain, 2 shot values, 10+ players
 	
 	public Board(){
 		super();
-		for (int x=0;x<1200;x++){
-			for (int y=0;y<500;y++){
+		for (int x=0;x<600;x++){
+			for (int y=0;y<250;y++){
 				board[x][y]=0;
 			}
 		}
 	}
 	
 	public  void terraingeneration(){
-		for (int x=0;x<200;x++){
-			for (int y=0;y<1200;y++){
+		for (int x=0;x<600;x++){
+			for (int y=150;y<250;y++){
 				board[x][y]=1;
 			}
 		}
 	}
 	
 	public void paintComponent(Graphics g){
-		
+		terraingeneration();
+		for (int x=0;x<600;x++){
+			for (int y=0;y<250;y++){
+				if (board[x][y]==1){
+					g.setColor(Color.GREEN);
+				}else{
+					g.setColor(new Color(0,240,240));
+				}
+				g.drawRect(x*2, y*2, 1, 1);
+			}
+		}
 	}
 	
 }
