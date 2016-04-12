f8fce32019869f6c4f1b7a5c14bc91bca751e1ec
Nicholas Zou <asdfjklundefined@gmail.com>
2016-04-12 12:51:52 -0400

    weaghbgmkwebgjl

--- ~Game~/src/Tester.java
+++ ~Game~/src/Tester.java
@@ -1,5 +1,39 @@
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
 
 
-public class Tester {
+public class Tester implements KeyListener{
 
+	static Tester test;
+	JFrame frame=new JFrame("Gottem");
+	static Board board=new Board();
+	
+	public Tester(){
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		board.setPreferredSize(new Dimension(1200,500));
+		frame.addKeyListener(this);
+
+		frame.add(board);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public void keyPressed(KeyEvent arg0) {
+		
+	}
+
+	public void keyReleased(KeyEvent arg0) {
+		
+	}
+
+	public void keyTyped(KeyEvent arg0) {
+		
+	}
+
+	public static void main (String[]args){
+		test =new Tester();
+	}
 }
