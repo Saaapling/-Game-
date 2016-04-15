dbad70f1de5e6268d1afd07afa7e912612bb1a98
Garn Piyasirisilp <garn28@gmail.com>
2016-04-15 12:57:21 -0400

    Cannonball , board, and tester

--- ~Game~/src/Board.java
+++ ~Game~/src/Board.java
@@ -1,13 +1,25 @@
-import java.awt.Color;
-import java.awt.Graphics;
 
-import javax.swing.JPanel;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicTreeUI.TreeIncrementAction;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.Color;
+import java.util.ArrayList;
 
 
 public class Board extends JPanel{
-	
+
 	public int[][] board = new int[600][250];  //0-Empty, 1 terrain, 2 shot values, 10+ players
-	
+
 	public Board(){
 		super();
 		for (int x=0;x<600;x++){
@@ -16,15 +28,26 @@ public class Board extends JPanel{
 			}
 		}
 	}
-	
+
 	public void drawtank(Tank tank, Graphics g){
-		g.drawLine(tank.xpos*2, tank.ypos*2, tank.xpos*2, tank.ypos*2+10);
-		g.drawLine(tank.xpos*2+1, tank.ypos*2, tank.xpos*2+1, tank.ypos*2+10);
-		g.drawLine(tank.xpos*2+2, tank.ypos*2+1, tank.xpos*2+2, tank.ypos*2+10);
-		g.drawLine(tank.xpos*2+3, tank.ypos*2+1, tank.xpos*2+3, tank.ypos*2+10);
-		g.drawLine(tank.xpos*2+4, tank.ypos*2+2, tank.xpos*2+4, tank.ypos*2+8);
+		g.drawLine(tank.xpos*2, tank.ypos*2, tank.xpos*2+10, tank.ypos*2);
+		g.drawLine(tank.xpos*2, tank.ypos*2-1, tank.xpos*2+10, tank.ypos*2-1);
+		g.drawLine(tank.xpos*2+1, tank.ypos*2-2, tank.xpos*2+10, tank.ypos*2-2);
+		g.drawLine(tank.xpos*2+1, tank.ypos*2-3, tank.xpos*2+9, tank.ypos*2-3);
+		g.drawLine(tank.xpos*2+2, tank.ypos*2-4, tank.xpos*2+8, tank.ypos*2-4);
+
+
+
 	}
-	
+
+	public void drawCannonball(Cannonball cannonball, Graphics g) {
+
+		g.fillOval(cannonball.xpos*3,200, 10, 10);
+
+		//g.fillArc(300,100,30,30,60,60);
+		
+	}
+
 	public void paintComponent(Graphics g){
 		for (int x=0;x<600;x++){
 			for (int y=0;y<250;y++){
@@ -36,6 +59,9 @@ public class Board extends JPanel{
 				g.drawRect(x*2, y*2, 1, 1);
 			}
 		}
+		g.setColor(Color.black);
+		drawtank(Tester.tank, g);
+		drawCannonball(Tester.cannonball, g);
 	}
-	
+
 }
--- ~Game~/src/Cannonball.java
+++ ~Game~/src/Cannonball.java
@@ -1,32 +1,46 @@
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import javax.swing.plaf.basic.BasicTreeUI.TreeIncrementAction;
-
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.awt.Color;
-import java.util.ArrayList;
-
-
 public class Cannonball {
 
 	double ballHorizontalSpeed;
 	double ballVerticalSpeed;
-	 
+
 	double ballSpeed;
 	double ballAngle;
-	
-	
-	
+
+	Cannonball cannonball;
+
+	Board board;
+
+	int xpos;				//Coordinate defined as bottom left hand corner of tank
+	int ypos;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	public Cannonball(int xstart, int ystart, Board newboard, int identity){
+		xpos=xstart;
+		ypos=ystart;
+		//id=identity;
+		board=newboard;
+		//boardadjust();
+	}
+
+	public void movement(int direction, int[][] board){
+		System.out.println("why garn?");
+		if (direction==1){
+			System.out.println("HA!");
+			//if (board[xpos+5][ypos]==0){
+			System.out.println("Whatta joke");
+			xpos+=1;
+			//}
+		}else{
+			System.out.println("Hm...");
+			//if (board[xpos-1][ypos]==0){
+			System.out.println("Cuz he's a scrub");
+			xpos-=1;
+			//}
+		}
 	}
 
 }
