131433ba9392bdc605814c025dd16386941c54c0
Nicholas Zou <asdfjklundefined@gmail.com>
2016-05-03 13:08:16 -0400

    Added HUD

---
+++ ~Game~/src/HUD.java
@@ -0,0 +1,17 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+
+public class HUD extends JPanel{
+	
+	Tank tank;
+	
+	public HUD(Tank newtank){
+		
+	}
+	
+	public void paintComponent(Graphics g){
+		
+	}
+	
+}
